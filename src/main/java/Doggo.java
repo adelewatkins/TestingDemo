a052c10d6980a2680a2492262c6c0080c57f9d42
adelewatkins <adele_watkins@hotmail.com>
2024-02-09 09:06:07 +0000

    doggo qa exercise

--- src/main/java/Doggo.java
+++ src/main/java/Doggo.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Doggo {
 
 	public String dogComp(int a) {
 
-		for (int i = 0; i <= 100; i++) {
+		for (int i = 1; i <= 100;) {
 
 			if (a == 11 || a == 12 || a == 13) {
 				return a + "th";
@@ -18,5 +20,34 @@ public class Doggo {
 
 		}
 		return null;
+	}
+
+	public List<String> compete(int place) {
+		List<String> placements = new ArrayList<>();
+
+		int lastDigit = -1;
+		String suffix = "";
+
+		for (int i = 1; i <= 100; i++) {
+			if (i == place) {
+				continue;
+			}
+			lastDigit = i % 10;
+
+			// modulus i by 100 if you want to go over 1,000
+			if (lastDigit == 1 && i != 11) {
+				suffix = "st";
+			} else if (lastDigit == 2 && i != 12) {
+				suffix = "nd";
+			} else if (lastDigit == 3 && i != 13) {
+				suffix = "rd";
+			} else {
+				suffix = "th";
+			}
+
+			placements.add(i + suffix);
+		}
+
+		return placements;
 	}
 }
--- src/test/java/DoggoTest.java
+++ src/test/java/DoggoTest.java
@@ -1,3 +1,5 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -5,9 +7,56 @@ public class DoggoTest {
 
 	private Doggo place = new Doggo();
 
+	private Doggo doggo = new Doggo();
+
 	@Test
 	void testDogComp() {
 		Assertions.assertEquals("23rd", this.place.dogComp(23));
+	}
+
+	@Test
+	void testSize() {
+		assertEquals(99, this.place.compete(42).size());
+	}
+
+	@Test
+	void testContains() {
+		assertEquals(false, this.doggo.compete(42).contains("42nd"));
+	}
+
+	@Test
+	void testFirst() {
+		assertEquals(true, this.doggo.compete(42).contains("21st"));
+	}
+
+	@Test
+	void testSecond() {
+		assertEquals(true, this.doggo.compete(42).contains("22nd"));
+	}
+
+	@Test
+	void testThird() {
+		assertEquals(true, this.doggo.compete(42).contains("23rd"));
+	}
+
+	@Test
+	void testFourth() {
+		assertEquals(true, this.doggo.compete(42).contains("24th"));
+	}
+
+	@Test
+	void testEleven() {
+		assertEquals(true, this.doggo.compete(42).contains("11th"));
+	}
+
+	@Test
+	void testTwelve() {
+		assertEquals(true, this.doggo.compete(42).contains("12th"));
+	}
+
+	@Test
+	void testThirteen() {
+		assertEquals(true, this.doggo.compete(42).contains("13th"));
 	}
 
 }
