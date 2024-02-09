af71f85f9c7f9b75953aeb12be01fd6b3c857f4d
adelewatkins <adele_watkins@hotmail.com>
2024-02-09 09:13:23 +0000

    siplified doggo exercise

--- src/main/java/Doggo.java
+++ src/main/java/Doggo.java
@@ -25,25 +25,22 @@ public class Doggo {
 	public List<String> compete(int place) {
 		List<String> placements = new ArrayList<>();
 
-		int lastDigit = -1;
-		String suffix = "";
-
 		for (int i = 1; i <= 100; i++) {
 			if (i == place) {
 				continue;
 			}
-			lastDigit = i % 10;
+			String suffix = "";
 
-			// modulus i by 100 if you want to go over 1,000
-			if (lastDigit == 1 && i != 11) {
-				suffix = "st";
-			} else if (lastDigit == 2 && i != 12) {
-				suffix = "nd";
-			} else if (lastDigit == 3 && i != 13) {
-				suffix = "rd";
-			} else {
+			if (i == 11 || i == 12 || i == 13) {
 				suffix = "th";
-			}
+			} else if (i % 10 == 3) {
+				suffix = "rd";
+			} else if (i % 10 == 2) {
+				suffix = "nd";
+			} else if (i % 10 == 1) {
+				suffix = "st";
+			} else
+				suffix = "th";
 
 			placements.add(i + suffix);
 		}
